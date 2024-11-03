84cec10d1ed34368b7efba219876d8dff01f002a
haiaza <ajames1342@gmail.com>
2024-11-03 15:45:47 -0500

    Ex 1

--- exercises.py
+++ exercises.py
@@ -15,3 +15,24 @@ def example_list_function():
 
 # Call the function and print each element
 example_list_function()
+
+
+# Exercise 1: List and Indexing
+#
+# Create a list named students containing at least three student names (strings).
+# Assign the second student’s name to a variable named first_student.
+# Assign the last student’s name to a variable named last_student.
+
+def manage_students():
+    # your code here
+
+    students = ['Nick', 'Alicia', 'Madison']
+
+    first_student = students[1]
+    second_student = students[2]
+
+    # print(first_student)
+    # print(second_student)
+
+# Call the function and print the result
+print('Exercise 1:', manage_students())
